06e95418430b527ebf86bf5b0ebfd007800e0c1e
ericmartins2010 <eric.martins2010@yahoo.com.br>
2023-03-13 21:40:55 -0300

    Refatorando a Classe Nó em Genérica

--- src/main/java/com/projetono/Main.java
+++ src/main/java/com/projetono/Main.java
@@ -3,15 +3,15 @@ package com.projetono;
 public class Main {
     public static void main(String[] args) {
 
-        No no1 = new No("Conteúdo nó1");
+        No<String> no1 = new No<>("Conteúdo nó1");
 
-        No no2 = new No("COnteúdo nó2");
+        No<String> no2 = new No<>("COnteúdo nó2");
         no1.setProximoNo(no2);
 
-        No no3 = new  No("COnteúdo nó3");
+        No<String> no3 = new  No<>("COnteúdo nó3");
         no2.setProximoNo(no3);
 
-        No no4 = new No("Conteúdo nó4");
+        No<String> no4 = new No<>("Conteúdo nó4");
         no3.setProximoNo(no4);
 
         //no1 -> no2 -> no3 -> no4 -> null
